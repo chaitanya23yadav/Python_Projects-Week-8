731566af9604a60e4ea3934edb144abc1f27f14c
chaitanya23yadav <115366177+chaitanya23yadav@users.noreply.github.com>
2023-04-30 23:34:51 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,7 @@
+from random import choice
+tup = ('!', '@', '#', '$', '%', '&', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+       'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+k = ''
+for i in range(12):
+    k = k + choice(tup)
+print(k)
